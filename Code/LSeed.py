2ab4857868155fc893af6c4f09f70013ad8f8fab
asifmansoor <asifmansoor.a@gmail.com>
2013-09-17 17:31:48 -0500

    Code for Items Count

---
+++ Code/LSeed.py
@@ -0,0 +1,40 @@
+
+import re
+from funcM import *
+
+from collections import Counter
+
+# Function for flattening lists of list
+def Listflat(Mainlist):
+    for itemset in Mainlist:
+        if type(itemset) in (list, tuple):
+            for item in Listflat(itemset):
+                yield item
+        else:
+            yield itemset
+# More easier line : list(itertools.chain(*strippedSeqItem))
+
+# Reading the transaction file
+Filename = '/Users/asifmansoor/MS-GSP/Data/data.txt'
+openFile = open(Filename)
+contents = openFile.read()
+
+# Splitting each line with delim 
+withline = contents.split("\r\n")
+
+# Extracting the numbers alone with reg ex and appending it to a list
+strippedSeqItem =[]
+
+for line in withline:
+    ItemList = re.sub(r'\D'," ",line).strip()
+    strippedSeqItem.append(ItemList.split())
+
+# A list containing all the items in the transaction, with orders preserverd
+itemsAll = list(Listflat(strippedSeqItem))
+#print itemsAll
+
+# Use counter data structure to hold all the items with its count values
+countItems = Counter(itemsAll)
+print countItems
+
+#Print M
